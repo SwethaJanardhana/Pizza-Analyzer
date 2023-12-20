db2591e5f3eafc390955a1f6983ff36f62253925
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2023-12-20 16:17:23 +0100

    Challenge Accomplished

--- js/index.js
+++ js/index.js
@@ -8,19 +8,51 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
+const pizzaElement1 = document.querySelector('[data-js="pizza-1"]');
+const pizzaElement2 = document.querySelector('[data-js="pizza-2"]');
+
+let pizzaSize1 = pizzaInput1.value;
+let pizzaSize2 = pizzaInput2.value;
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  pizzaSize1 = pizzaInput1.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizzaElement1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizzaElement2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
 
+function calculatePizzaGain(diameter1, diameter2) {
+  const radius1 = diameter1 / 2;
+  const radius2 = diameter2 / 2;
+  const area1 = Math.PI * radius1 * radius1;
+  const area2 = Math.PI * radius2 * radius2;
+  let pizzaGain = ((area2 - area1) / area1) * 100;
+  output.textContent = Math.round(pizzaGain);
+}
+
 // Task 2
 // define the function updatePizzaDisplay here
 
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newDisplay = (newSize / 24) * 100;
+  pizzaElement.style.width = newDisplay + "px";
+}
+
 // Task 3
 // define the function updateOutputColor here
+function updateOutputColor(size1, size2) {
+  if (size1 > size2) {
+    outputSection.classList.add("output-display__loose");
+  } else {
+    outputSection.classList.remove("output-display__loose");
+  }
+}
